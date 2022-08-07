fae41578e10c64a64f663616cbf6f9cb6a704e44
unknown <lyotin44@gmail.com>
2022-08-07 21:06:18 +0300

    Update Create post from class to functional component

--- src/components/PostData/CreatePost/CreatePost.tsx
+++ src/components/PostData/CreatePost/CreatePost.tsx
@@ -1,90 +1,62 @@
-import React from "react";
 //Redux
-import { connect } from "react-redux";
-import { createPost, cleanErrors } from "../../../redux/actions/dataActions";
-import { CreatePostProps, CreatePostState } from "./CreatePostInterfaces";
+import { useDispatch, useSelector } from "react-redux";
+import { createPost } from "../../../redux/actions/dataActions";
+import { CreatePostProps } from "./CreatePostInterfaces";
 
-class CreatePost extends React.Component<CreatePostProps> {
-	state: CreatePostState = {
-		open: false,
-		body: "",
-		errors: { body: "" },
-	};
+const CreatePost = () => {
+	const mapStateToProps = (state: CreatePostProps) => ({
+		user: state.user,
+		UI: state.UI,
+	});
 
-	static getDerivedStateFromProps(
-		prevProps: CreatePostProps,
-		prevState: CreatePostState
-	) {
-		const prevStateData = {
-			body: prevState.body,
-			open: prevState.open,
-			errors: prevState.errors,
-		};
-		if (prevProps.UI)
-			if (prevProps.UI.errors) return { errors: prevProps.UI.errors };
-		if (!prevProps.UI.errors && !prevProps.UI.loading) {
-			prevState.body = "";
-			prevState.open = false;
-			prevState.errors.body = "";
-			return prevStateData;
-		}
+	const data = useSelector(mapStateToProps);
+	const dispatch = useDispatch();
 
-		return null;
-	}
-
-	handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-		this.setState({ [event.target.name]: event.target.value });
-	};
-
-	handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+	const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		this.props.createPost({ body: this.state.body });
-		this.setState({
-			body: "",
-		});
+
+		const formData = new FormData(event.currentTarget);
+		const body = formData.get("body");
+
+		dispatch(createPost({ body: String(body) }));
+		event.currentTarget.reset();
 	};
-	render() {
-		const { errors } = this.state;
 
-		return this.props.user?.credentials.username ? (
-			<form className="form-post" onSubmit={this.handleSubmit}>
-				<div className="form__body">
-					<div className="form__row">
-						<div className="form__cols">
-							<div className="form__col">
-								<div className="form__controls">
-									<textarea
-										name="body"
-										placeholder="Write your post"
-										onChange={this.handleChange}
-									/>
+	const {
+		user,
+		UI: { errors },
+	} = data;
 
-									<span className="form__login-error">
-										{errors.body}
-									</span>
-								</div>
+	return user?.credentials.username ? (
+		<form className="form-post" onSubmit={handleSubmit}>
+			<div className="form__body">
+				<div className="form__row">
+					<div className="form__cols">
+						<div className="form__col">
+							<div className="form__controls">
+								<textarea
+									name="body"
+									placeholder="Write your post"
+								/>
+
+								<span className="form__login-error">
+									{errors ? errors.body : ""}
+								</span>
 							</div>
 						</div>
 					</div>
 				</div>
+			</div>
 
-				<div className="form__actions">
-					<button type="submit" className="btn">
-						Submit
-					</button>
-				</div>
-			</form>
-		) : (
-			<></>
-		);
-	}
-}
+			<div className="form__actions">
+				<button type="submit" className="btn">
+					Submit
+				</button>
+			</div>
+		</form>
+	) : (
+		<></>
+	);
+};
 
-const mapStateToProps = (state: CreatePostProps) => ({
-	UI: state.UI,
-	user: state.user,
-});
-
-export default connect(mapStateToProps, { createPost, cleanErrors })(
-	CreatePost
-);
+export default CreatePost;
