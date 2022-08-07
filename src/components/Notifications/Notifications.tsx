fdf1a10f495c0e5ca810079a3c35884ac3b85891
unknown <lyotin44@gmail.com>
2022-08-07 19:48:50 +0300

    Update Notifications code from class component to function component

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 //Dayjs
 import dayjs from "dayjs";
@@ -6,16 +6,13 @@ import relativeTime from "dayjs/plugin/relativeTime";
 //Muinpm start
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import IconButton from "@material-ui/core/IconButton";
-import Tooltip from "@material-ui/core/Tooltip";
-import Typography from "@material-ui/core/Typography";
 import Badge from "@material-ui/core/Badge";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import ChatIcon from "@material-ui/icons/Chat";
 
 //Redux
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { markNotificationsRead } from "../../redux/actions/userActions";
 
 interface DateObj {
@@ -32,129 +29,124 @@ interface NotificationsData {
 	read: boolean;
 	notificationId: string;
 }
-interface Props {
-	markNotificationsRead: (notificationIds: string[]) => void;
-	notifications: NotificationsData[];
-}
 
 interface StateToPropsData {
 	user: { notifications: NotificationsData[] };
 }
-class Notifications extends Component<Props> {
-	state = {
-		anchorEl: null,
-	};
-	handleOpen = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-		this.setState({
-			anchorEl: event.target,
-		});
+
+const Notifications = () => {
+	const [anchorEl, setAnchorEl] = useState<any>(null);
+
+	const mapStateToProps = (state: StateToPropsData) => ({
+		notifications: state.user.notifications,
+	});
+
+	const data = useSelector(mapStateToProps);
+	const dispatch = useDispatch();
+
+	const handleOpen = (
+		event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+	) => {
+		setAnchorEl(event.target);
 	};
 
-	handleClose = () => {
-		this.setState({
-			anchorEl: null,
-		});
+	const handleClose = () => {
+		setAnchorEl(null);
 	};
 
-	onMenuOpened = () => {
-		let unreadNotificationsIds = this.props.notifications
+	const onMenuOpened = () => {
+		let unreadNotificationsIds = data.notifications
 			.filter((not) => !not.read)
 			.map((not) => not.notificationId);
-		this.props.markNotificationsRead(unreadNotificationsIds);
+
+		dispatch(markNotificationsRead(unreadNotificationsIds));
 	};
-	render() {
-		dayjs.extend(relativeTime);
-		const notifications = this.props.notifications;
-		const anchorEl = this.state.anchorEl;
-		let notificationsIcon;
-		if (notifications && notifications.length > 0) {
-			notifications.filter((not) => not.read === false).length > 0
-				? (notificationsIcon = (
-						<Badge
-							badgeContent={
-								notifications.filter(
-									(not) => not.read === false
-								).length
-							}
-							color="secondary"
-						>
-							<NotificationsIcon />
-						</Badge>
-				  ))
-				: (notificationsIcon = <NotificationsIcon />);
-		} else notificationsIcon = <NotificationsIcon />;
 
-		let notificationsMarkup =
-			notifications && notifications.length > 0 ? (
-				notifications.map((not) => {
-					const verb = not.type === "like" ? "liked" : "commented on";
-					const time = dayjs(
-						new Date(not.createdAt._seconds * 1000)
-					).fromNow();
-					const iconColor = not.read ? "primary" : "secondary";
-					const icon =
-						not.type === "like" ? (
-							<FavoriteIcon
-								color={iconColor}
-								style={{ marginRight: 10 }}
-							/>
-						) : (
-							<ChatIcon
-								color={iconColor}
-								style={{ marginRight: 10 }}
-							/>
-						);
-					return (
-						<MenuItem
-							key={not.createdAt._seconds}
-							onClick={this.handleClose}
-							className="notification"
-						>
-							<Link to={`/post/${not.postId}`}>{icon}</Link>
+	dayjs.extend(relativeTime);
+	const notifications = data.notifications;
+	let notificationsIcon;
 
-							<Link to={`/post/${not.postId}`}>
-								<Typography color="inherit" variant="body1">
-									{`${not.sender} ${verb} your post ${time}`}
-								</Typography>
-							</Link>
-						</MenuItem>
+	if (notifications && notifications.length > 0) {
+		notifications.filter((not: NotificationsData) => not.read === false)
+			.length > 0
+			? (notificationsIcon = (
+					<Badge
+						badgeContent={
+							notifications.filter(
+								(not: NotificationsData) => not.read === false
+							).length
+						}
+						color="secondary"
+					>
+						<NotificationsIcon />
+					</Badge>
+			  ))
+			: (notificationsIcon = <NotificationsIcon />);
+	} else notificationsIcon = <NotificationsIcon />;
+
+	let notificationsMarkup =
+		notifications && notifications.length > 0 ? (
+			notifications.map((not: NotificationsData) => {
+				const verb = not.type === "like" ? "liked" : "commented on";
+				const time = dayjs(
+					new Date(not.createdAt._seconds * 1000)
+				).fromNow();
+				const iconColor = not.read ? "primary" : "secondary";
+				const icon =
+					not.type === "like" ? (
+						<FavoriteIcon color={iconColor} />
+					) : (
+						<ChatIcon color={iconColor} />
 					);
-				})
-			) : (
-				<MenuItem onClick={this.handleClose}>
-					You have no notifications yet
-				</MenuItem>
-			);
-
-		return (
-			<Fragment>
-				<Tooltip placement="top" title="">
-					<IconButton
-						aria-owns={anchorEl ? "simple-menu" : undefined}
-						aria-haspopup="true"
-						onClick={this.handleOpen}
+				return (
+					<MenuItem
+						key={not.createdAt._seconds}
+						onClick={handleClose}
 						className="notification"
 					>
-						{notificationsIcon}
-					</IconButton>
-				</Tooltip>
-				<Menu
-					style={{ marginTop: 40 }}
-					anchorEl={anchorEl}
-					open={Boolean(anchorEl)}
-					onClose={this.handleClose}
-					onEntered={this.onMenuOpened}
-				>
-					{notificationsMarkup}
-				</Menu>
-			</Fragment>
-		);
-	}
-}
-const mapStateToProps = (state: StateToPropsData) => ({
-	notifications: state.user.notifications,
-});
+						<Link
+							className="notification__icon"
+							to={`/post/${not.postId}`}
+						>
+							{icon}
+						</Link>
 
-export default connect(mapStateToProps, { markNotificationsRead })(
-	Notifications
-);
+						<Link
+							className="notification__content"
+							to={`/post/${not.postId}`}
+						>
+							<p>{`${not.sender} ${verb} your post ${time}`}</p>
+						</Link>
+					</MenuItem>
+				);
+			})
+		) : (
+			<MenuItem onClick={handleClose}>
+				You have no notifications yet
+			</MenuItem>
+		);
+
+	return (
+		<>
+			<button
+				aria-owns={anchorEl ? "simple-menu" : undefined}
+				aria-haspopup="true"
+				onClick={handleOpen}
+				className="btn-notification"
+			>
+				{notificationsIcon}
+			</button>
+			<Menu
+				open={Boolean(anchorEl)}
+				anchorEl={anchorEl}
+				onClose={handleClose}
+				onEntered={onMenuOpened}
+				className="notifications"
+			>
+				{notificationsMarkup}
+			</Menu>
+		</>
+	);
+};
+
+export default Notifications;
