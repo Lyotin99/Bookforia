0556b6515bd05dbc58fadaeb30111c121c5ca543
unknown <lyotin44@gmail.com>
2022-08-09 15:29:31 +0300

    Update component BooksLibrary from class to functional

--- src/pages/BooksLibrary/BooksLibrary.tsx
+++ src/pages/BooksLibrary/BooksLibrary.tsx
@@ -1,134 +1,97 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import BookSpinner from "../../photos/bookSpinner.svg";
 import SliderBooks from "../../components/SliderBooks/SliderBooks";
 import FormBooks from "../../components/FormBooks/FormBooks";
+import booksService from "../../services/booksService";
 
-class BooksLibrary extends Component {
-	state = {
-		books: [],
-		reviewedBooksPaperback: [],
-		reviewedBooksNonFictional: [],
-		reviewedBooksFictional: [],
-		bookSearchFilter: "",
-		formIsClicked: false,
+const BooksLibrary = () => {
+	const [reviewedBooksFiction, setReviewedBooksFiction] = useState([]);
+	const [reviewedBooksNonFiction, setReviewedBooksNonFiction] = useState([]);
+	const [reviewedBooksPaperback, setReviewedBooksPaperBack] = useState([]);
+	const [formIsClicked, setFormIsClicked] = useState<boolean>(false);
+
+	useEffect(() => {
+		const hardcoverFiction = booksService("hardcover-fiction");
+		const hardcoverNonFiction = booksService("hardcover-nonfiction");
+		const paperbackNonFiction = booksService("paperback-nonfiction");
+
+		hardcoverFiction.then((data) => {
+			setReviewedBooksFiction(data);
+		});
+
+		hardcoverNonFiction.then((data) => {
+			setReviewedBooksNonFiction(data);
+		});
+
+		paperbackNonFiction.then((data) => {
+			setReviewedBooksPaperBack(data);
+		});
+	}, []);
+
+	let handleFormSubmitted = (isClicked: boolean) => {
+		if (isClicked) {
+			setFormIsClicked(true);
+		}
 	};
 
-	componentDidMount() {
-		let apiKey = "QyzWBJom9uRSHkNkVZvMT5xLnnjRqPgV";
-		fetch(
-			"https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?&api-key=" +
-				apiKey,
-			{ method: "get" }
-		)
-			.then((response) => {
-				return response.json();
-			})
-			.then((json) => {
-				if (json.results)
-					this.setState({
-						reviewedBooksFictional: json.results.books,
-					});
-			});
-		fetch(
-			"https://api.nytimes.com/svc/books/v3/lists/current/hardcover-nonfiction.json?&api-key=" +
-				apiKey,
-			{ method: "get" }
-		)
-			.then((response) => {
-				return response.json();
-			})
-			.then((json) => {
-				if (json.results)
-					this.setState({
-						reviewedBooksNonFictional: json.results.books,
-					});
-			});
-		fetch(
-			"https://api.nytimes.com/svc/books/v3/lists/current/paperback-nonfiction.json?&api-key=" +
-				apiKey,
-			{ method: "get" }
-		)
-			.then((response) => {
-				return response.json();
-			})
-			.then((json) => {
-				if (json.results)
-					this.setState({
-						reviewedBooksPaperback: json.results.books,
-					});
-			});
-	}
+	const reviewedBooksMarkup = (
+		<SliderBooks reviewedBooks={reviewedBooksFiction} />
+	);
+	const reviewedBooksNonFictional = (
+		<SliderBooks reviewedBooks={reviewedBooksNonFiction} />
+	);
+	const reviewedBooksPaperbackNonfiction = (
+		<SliderBooks reviewedBooks={reviewedBooksPaperback} />
+	);
 
-	render() {
-		let handleFormSubmitted = (isClicked: boolean) => {
-			if (isClicked) {
-				return this.setState({
-					formIsClicked: true,
-				});
-			}
-		};
+	let allCategoriesFetched =
+		reviewedBooksFiction.length > 0 &&
+		reviewedBooksNonFiction.length > 0 &&
+		reviewedBooksPaperback.length > 0;
 
-		const reviewedBooksMarkup = (
-			<SliderBooks reviewedBooks={this.state.reviewedBooksFictional} />
-		);
-		const reviewedBooksNonFictional = (
-			<SliderBooks reviewedBooks={this.state.reviewedBooksNonFictional} />
-		);
-		const reviewedBooksPaperback = (
-			<SliderBooks reviewedBooks={this.state.reviewedBooksPaperback} />
-		);
-
-		let booksFetched =
-			this.state.reviewedBooksFictional.length > 0 &&
-			this.state.reviewedBooksNonFictional.length > 0 &&
-			this.state.reviewedBooksPaperback.length > 0;
-
-		return (
-			<div className="section-sliders">
-				<div className="shell">
-					<div className="section__inner">
-						<div className="section__search-books">
-							<FormBooks
-								handleFormSubmitted={handleFormSubmitted}
-							/>
-						</div>
-
-						{!this.state.formIsClicked ? (
-							booksFetched ? (
-								<div className="section__sliders">
-									<div className="section__slider">
-										<h2>Hardcover Fiction</h2>
-
-										{reviewedBooksMarkup}
-									</div>
-
-									<div className="section__slider">
-										<h2>Hardcover non-fiction</h2>
-
-										{reviewedBooksNonFictional}
-									</div>
-
-									<div className="section__slider">
-										<h2>Paperback non-fictional</h2>
-
-										{reviewedBooksPaperback}
-									</div>
-								</div>
-							) : (
-								<img
-									className="spinner"
-									src={BookSpinner}
-									alt="Book Spinner"
-								/>
-							)
-						) : (
-							""
-						)}
+	return (
+		<div className="section-sliders">
+			<div className="shell">
+				<div className="section__inner">
+					<div className="section__search-books">
+						<FormBooks handleFormSubmitted={handleFormSubmitted} />
 					</div>
+
+					{!formIsClicked ? (
+						allCategoriesFetched ? (
+							<div className="section__sliders">
+								<div className="section__slider">
+									<h2>Hardcover Fiction</h2>
+
+									{reviewedBooksMarkup}
+								</div>
+
+								<div className="section__slider">
+									<h2>Hardcover non-fiction</h2>
+
+									{reviewedBooksNonFictional}
+								</div>
+
+								<div className="section__slider">
+									<h2>Paperback non-fictional</h2>
+
+									{reviewedBooksPaperbackNonfiction}
+								</div>
+							</div>
+						) : (
+							<img
+								className="spinner"
+								src={BookSpinner}
+								alt="Book Spinner"
+							/>
+						)
+					) : (
+						""
+					)}
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default BooksLibrary;
