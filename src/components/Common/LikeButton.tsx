331f79da00d2a73d40c70b0e7a0dad08e2572f7f
unknown <lyotin44@gmail.com>
2022-08-12 12:10:27 +0300

    Update component LikeButton: from class to functional

--- src/components/Common/LikeButton.tsx
+++ src/components/Common/LikeButton.tsx
@@ -1,70 +1,58 @@
-import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import useReduxSelector from "../../hooks/useReduxSelector";
 //MUI
 import { Favorite, FavoriteBorder } from "@material-ui/icons";
 //Redux
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import { likePost, unlikePost } from "../../redux/actions/postActions";
 //Interfaces
-import { Posts, LikesData } from "../../utils/Interfaces";
+import { LikesData } from "../../utils/Interfaces";
 
-interface Props {
-	likePost?: (postId: string) => void;
-	unlikePost?: (postId: string) => void;
-	postId: string;
-	user: Posts;
-}
+const LikeButton = (props: { postId: string }) => {
+	const data = useReduxSelector();
+	const dispatch = useDispatch();
+	const { user } = data;
 
-export class LikeButton extends Component<Props> {
-	likedPost = () => {
+	const likedPost = () => {
 		if (
-			this.props.user &&
-			this.props.user.likes &&
-			this.props.user.likes.find(
-				(like: LikesData) => like.postId === this.props.postId
-			)
+			user &&
+			user.likes &&
+			user.likes.find((like: LikesData) => like.postId === props.postId)
 		) {
 			return true;
 		} else return false;
 	};
 
-	likePost = () => {
-		if (this.props.likePost) this.props.likePost(this.props.postId);
+	const likePostHandler = () => {
+		dispatch(likePost(props.postId));
 	};
-	unlikePost = () => {
-		if (this.props.unlikePost) this.props.unlikePost(this.props.postId);
-	};
-	render() {
-		const { user } = this.props;
 
-		const likeButton =
-			user && !user.authenticated ? (
-				<Link to="/login">
-					<div title="Like">
-						<button>
-							<FavoriteBorder color="primary" />
-						</button>
-					</div>
-				</Link>
-			) : this.likedPost() ? (
-				<div title="Unlike">
-					<button onClick={this.unlikePost}>
-						<Favorite color="primary" />
+	const unlikePostHandler = () => {
+		dispatch(unlikePost(props.postId));
+	};
+
+	const likeButton =
+		user && !user.authenticated ? (
+			<Link to="/login">
+				<div title="Like">
+					<button>
+						<FavoriteBorder color="primary" />
 					</button>
 				</div>
-			) : (
-				<button title="Like" onClick={this.likePost}>
-					<FavoriteBorder color="primary" />
+			</Link>
+		) : likedPost() ? (
+			<div title="Unlike">
+				<button onClick={unlikePostHandler}>
+					<Favorite color="primary" />
 				</button>
-			);
-		return likeButton;
-	}
-}
-const mapActionsToProps = {
-	likePost,
-	unlikePost,
+			</div>
+		) : (
+			<button title="Like" onClick={likePostHandler}>
+				<FavoriteBorder color="primary" />
+			</button>
+		);
+
+	return likeButton;
 };
-const mapStateToProps = (state: Props) => ({
-	user: state.user,
-});
-export default connect(mapStateToProps, mapActionsToProps)(LikeButton);
+
+export default LikeButton;
