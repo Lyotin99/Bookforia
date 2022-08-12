7391ec0b20ea8536791b25d28567be11150e92ae
unknown <lyotin44@gmail.com>
2022-08-12 12:10:43 +0300

    Update component SaveButton: from class to functional

--- src/components/Common/SaveButton.tsx
+++ src/components/Common/SaveButton.tsx
@@ -1,58 +1,53 @@
-import React from "react";
+import useReduxSelector from "../../hooks/useReduxSelector";
 //MUI
 import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
 import BookmarkIcon from "@material-ui/icons/Bookmark";
 //REDUX
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import { savePost, unSavePost } from "../../redux/actions/postActions";
 //Interfaces
-import { Posts } from "../../utils/Interfaces";
-interface Props {
-	postId: string;
-	savePost?: (postId: string) => void;
-	unSavePost?: (postId: string) => void;
-	user: Posts;
-}
+import { LikesData } from "../../utils/Interfaces";
 
-class SaveButton extends React.Component<Props> {
-	savedPost = () => {
+const SaveButton = (props: { postId: string }) => {
+	const data = useReduxSelector();
+	const dispatch = useDispatch();
+	const {
+		user: { savedPosts },
+	} = data;
+
+	const savedPost = () => {
 		if (
-			this.props.user.savedPosts.find(
-				(savedPost: any) => savedPost.postId === this.props.postId
+			savedPosts.find(
+				(savedPost: LikesData) => savedPost.postId === props.postId
 			)
 		) {
 			return true;
 		} else return false;
 	};
 
-	likePost = () => {
-		if (this.props.savePost) this.props.savePost(this.props.postId);
+	const likePostHandler = () => {
+		dispatch(savePost(props.postId));
 	};
-	unlikePost = () => {
-		if (this.props.unSavePost) this.props.unSavePost(this.props.postId);
+
+	const unlikePostHandler = () => {
+		dispatch(unSavePost(props.postId));
 	};
-	render() {
-		const savedBtn = this.savedPost() ? (
-			<div title="Unsave post">
-				<button onClick={this.unlikePost}>
-					<BookmarkIcon color="primary" fontSize="small" />
-				</button>
-			</div>
-		) : (
-			<div title="Save post">
-				<button onClick={this.likePost}>
-					<BookmarkBorderIcon color="primary" fontSize="small" />
-				</button>
-			</div>
-		);
-		return savedBtn;
-	}
-}
-const mapActionsToProps = {
-	savePost,
-	unSavePost,
+
+	const savedBtn = savedPost() ? (
+		<div title="Unsave post">
+			<button onClick={likePostHandler}>
+				<BookmarkIcon color="primary" fontSize="small" />
+			</button>
+		</div>
+	) : (
+		<div title="Save post">
+			<button onClick={unlikePostHandler}>
+				<BookmarkBorderIcon color="primary" fontSize="small" />
+			</button>
+		</div>
+	);
+
+	return savedBtn;
 };
-const mapStateToProps = (state: Props) => ({
-	user: state.user,
-});
-export default connect(mapStateToProps, mapActionsToProps)(SaveButton);
+
+export default SaveButton;
