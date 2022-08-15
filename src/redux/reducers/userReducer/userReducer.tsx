ca177f1312fb4cb7297ff8665de1de9a5e6984b5
unknown <lyotin44@gmail.com>
2022-08-15 23:42:36 +0300

    Update redux types

--- src/redux/reducers/userReducer/userReducer.tsx
+++ src/redux/reducers/userReducer/userReducer.tsx
@@ -1,3 +1,4 @@
+import { UserData } from "../../../utils/Interfaces";
 import { Actions } from "../../types";
 
 import { initialStateProps } from "./userReducerInterfaces";
@@ -11,7 +12,57 @@ const initialState: initialStateProps = {
 	notifications: [],
 };
 
-const userData = (state = initialState, action: any) => {
+type ActionType =
+	| isAuthenticated
+	| SetUser
+	| LoadingUser
+	| SavePost
+	| UnSavePost
+	| LikneUnlikePost
+	| MarkNotifications;
+
+interface isAuthenticated {
+	type: Actions.SET_AUTHENTICATED | Actions.SET_UNAUTHENTICATED;
+}
+
+interface SetUser {
+	type: Actions.SET_USER;
+	payload: UserData;
+}
+
+interface LoadingUser {
+	type: Actions.LOADING_USER;
+}
+
+interface SavePost {
+	type: Actions.SAVE_POST;
+	payload: {
+		postId: string;
+		username: string;
+	};
+}
+
+interface UnSavePost {
+	type: Actions.UNSAVE_POST;
+	payload: {
+		savedPostId: string;
+		postId: string;
+	};
+}
+
+interface LikneUnlikePost {
+	type: Actions.LIKE_POST | Actions.UNLIKE_POST;
+	payload: {
+		postId: string;
+		username: string;
+	};
+}
+
+interface MarkNotifications {
+	type: Actions.MARK_NOTIFICATIONS_READ;
+}
+
+const userData = (state = initialState, action: ActionType) => {
 	switch (action.type) {
 		case Actions.SET_AUTHENTICATED:
 			return {
@@ -22,9 +73,9 @@ const userData = (state = initialState, action: any) => {
 			return initialState;
 		case Actions.SET_USER:
 			return {
+				...action.payload,
 				authenticated: true,
 				loading: false,
-				...action.payload,
 			};
 		case Actions.LOADING_USER:
 			return {
